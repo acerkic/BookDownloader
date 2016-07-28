e18d1015c3f55725eb83906e7d1110df19441206
Amer <amer.cerkic@outlook.com>
2016-07-27 21:23:58 -0700

    Add project files.

---
+++ BookDownload/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookDownload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpClient client = new HttpClient();
+
+            using (client = new HttpClient())
+            {
+                Console.WriteLine("Getting Book Catalog");
+                HttpResponseMessage response = client.GetAsync("http://www.mssmallbiz.com/ericligman/Key_Shorts/MSFTFreeEbooks.txt").Result;
+
+                response.EnsureSuccessStatusCode();
+
+                string result = response.Content.ReadAsStringAsync().Result;
+
+                string[] books = result.Replace('\r',' ').Split('\n').Skip(1).ToArray();
+                string localFolder = "c:\\temp\\";
+
+                Console.WriteLine("Starting Book Download");
+                CancellationToken token = new CancellationToken(false);
+                Parallel.ForEach(books, new ParallelOptions { CancellationToken = token }, (bookurl) =>
+                {
+                    
+                    var bookResponse = client.GetAsync(bookurl).Result;
+                    string bookName = bookResponse.RequestMessage.RequestUri.Segments[bookResponse.RequestMessage.RequestUri.Segments.Length - 1];
+                    DownloadFile(bookurl, string.Concat(localFolder,bookName));
+                    Console.WriteLine("{0} downloaded",bookName);
+                });
+
+                Console.Write("Book Download Complete");
+            }
+        }
+
+
+        public static void DownloadFile(string url, string filePath)
+        {
+            WebClient cli = new WebClient();
+            cli.DownloadFile(new Uri(url), filePath);
+            
+        }
+
+     
+    }
+}
